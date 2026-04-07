c01a54cc3d2ed2d992af4c065c62b77a6d2e9857
SriJainthu <kannappanlatha00@gmail.com>
2026-04-07 14:16:08 +0530

    Update UI

--- src/App.js
+++ src/App.js
@@ -2,23 +2,7 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>CI/CD is Working 🎉</h1>
-      <p>This app is automatically deployed using GitHub Actions and Vercel</p>
-
-      <h2>Services</h2>
-      <ul>
-        <li>Plumbing</li>
-        <li>Wiring</li>
-        <li>Cleaning</li>
-        <li>Swimming</li>
-      </ul>
-
-      <h2>Users</h2>
-      <ul>
-        <li>Leanne Graham</li>
-        <li>Ervin Howell</li>
-        <li>Clementine Bauch</li>
-        <li>Patricia Lebsack</li>
-      </ul>
+      <p>This app is automatically deployed</p>
     </div>
   );
 }
